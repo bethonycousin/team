d9fa8f0ff3056f00c34514240fa53b3f28d4e8d3
bethonycousin <80657735+bethonycousin@users.noreply.github.com>
2021-03-21 21:51:09 -0400

    Add files via upload

---
+++ CheckingAccount.py
@@ -0,0 +1,19 @@
+class CheckingAccount:
+
+    def __init__(self,name,address,accountnumber,balance):
+        self.__name = name;
+        self.__address = address
+        self.__acoountNumber = accountnumber
+        self.__balance = balance
+
+    def credtitAccount(self,amount):
+        self.__balance = self.__balance + amount
+    def debitAccount(self,amount):
+        if self.__balance < amount:
+            print("Balance (${:.2f}) is low. Debit operation can not be performed for amount ${:.2f}!!!".format(self.__balance,amount))
+        else:
+            self.__balance = self.__balance - amount
+    def showBalance(self):
+        print("Balance of the account number {} is ${:.2f}, which is in name of {}.".format(self.__acoountNumber,self.__balance,self.__name))
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from CheckingAccount import CheckingAccount
+
+account1 = CheckingAccount("Bethony Cousin","123 elmore street",1234,60.8)
+
+account1.credtitAccount(23.80)
+account1.debitAccount(350.00)
+account1.debitAccount(75.00)
+
+account1.showBalance()
